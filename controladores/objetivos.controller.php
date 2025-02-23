c8390418d94e857942d33eead08aabd098cc4bfa
Javier Pineda <javierpineda85@gmail.com>
2025-02-23 20:55:42 -0300

    Creación de modelo y controlador de objetivo
    Se creó el modelo y controlador para crear y editar los objetivos

---
+++ controladores/objetivos.controller.php
@@ -0,0 +1,96 @@
+<?php
+ob_start(); //permite enviar los headers sin interferencias
+require_once('modelos/objetivos.php');
+
+class ControladorObjetivos
+{
+
+    /*GUARDAR OBJETIVOS */
+    static public function crtGuardarObjetivo()
+    {
+        if (isset($_POST["nombreObjetivo"])) {
+
+            try {
+                $conexion = Conexion::conectar();
+
+                // Verificar si ya hay una transacción activa
+                if (!$conexion->inTransaction()) {
+                    // Si no hay una transacción activa, iniciar una nueva
+                    $conexion->beginTransaction();
+                }
+
+                $tabla = "objetivos";
+
+                $datos = array(
+                    "nombre" => $_POST["nombreObjetivo"],
+                    "localidad" => $_POST["localidad"],
+                    "referente" => $_POST["referente"],
+                    "tipo" => $_POST["tipo"]
+                );
+
+                ModeloObjetivos::mdlGuardarObjetivo($tabla, $datos);
+
+                // Confirmar la transacción si no hay errores
+                Conexion::conectar()->commit();
+
+                $_SESSION['success_message'] = 'Objetivo creado exitosamente';
+            } catch (Exception $e) {
+                // Revertir la transacción en caso de error
+                Conexion::conectar()->rollBack();
+
+                // Manejar el error según sea necesario
+                $_SESSION['success_message'] =  $e->getMessage();
+
+                return false;
+            }
+        }
+    }
+
+    /* MODIFICAR OBJETIVO */
+    static public function crtModificarObjetivo()
+    {
+        if (isset($_POST["nombre"])) {
+
+            try {
+                $conexion = Conexion::conectar();
+
+                // Iniciar una transacción
+                if (!$conexion->inTransaction()) {
+                    $conexion->beginTransaction();
+                }
+
+                $tabla = "objetivos";
+
+                $datos = array(
+                    "idObjetivo"  => $_POST["idObjetivo"],
+                    "nombre"      => $_POST["nombre"],
+                    "localidad"   => $_POST["localidad"],
+                    "referente"   => $_POST["referente"],
+                    "tipo"        => $_POST["tipo"]
+                );
+
+                $respuesta = ModeloObjetivos::mdlModificarObjetivo($tabla, $datos);
+
+                if ($respuesta === "ok") {
+                    // Confirmar la transacción
+                    $conexion->commit();
+                    $_SESSION['success_message'] = 'Objetivo modificado exitosamente';
+                    header("Location:?r=listado_objetivos");
+                    exit;
+                } else {
+                    // Si algo falla, hacer rollback
+                    $conexion->rollBack();
+                    $_SESSION['error_message'] = 'Error al modificar el objetivo';
+                    header("Location: ?r=editar_objetivo.php&id=" . $_POST["idObjetivo"]);
+                    exit;
+                }
+            } catch (Exception $e) {
+                // En caso de error, revertir la transacción
+                $conexion->rollBack();
+                $_SESSION['error_message'] = "Error: " . $e->getMessage();
+                header("Location: ?r=editar_objetivo.php&id=" . $_POST["idObjetivo"]);
+                exit;
+            }
+        }
+    }
+}
---
+++ modelos/objetivos.php
@@ -0,0 +1,53 @@
+<?php
+
+class ModeloObjetivos
+{
+    /*INSERTAR OBJETIVO */
+    static public function mdlGuardarObjetivo($tabla, $datos)
+    {
+        $registro = Conexion::conectar()->prepare("INSERT INTO $tabla (nombre, localidad, referente, tipo) 
+            VALUES (:nombre, :localidad, :referente, :tipo)");
+
+        $registro->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $registro->bindParam(":localidad", $datos["localidad"], PDO::PARAM_STR);
+        $registro->bindParam(":referente", $datos["referente"], PDO::PARAM_STR);
+        $registro->bindParam(":tipo", $datos["tipo"], PDO::PARAM_STR);
+
+        if ($registro->execute()) {
+
+            return "ok";
+        } else {
+            print_r(Conexion::conectar()->errorInfo());
+        }
+
+        $registro->closeCursor();
+        $registro = null;
+    }
+
+
+    /*MODIFICAR OBJETIVO */
+    static public function mdlModificarObjetivo($tabla, $datos)
+    {
+        try {
+            $conexion = Conexion::conectar();
+            $sql = "UPDATE $tabla SET nombre = :nombre, localidad = :localidad, 
+                    referente = :referente, tipo = :tipo WHERE idObjetivo = :idObjetivo";
+    
+            $stmt = $conexion->prepare($sql);
+    
+            $stmt->bindParam(":idObjetivo", $datos["idObjetivo"], PDO::PARAM_INT);
+            $stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+            $stmt->bindParam(":localidad", $datos["localidad"], PDO::PARAM_STR);
+            $stmt->bindParam(":referente", $datos["referente"], PDO::PARAM_STR);
+            $stmt->bindParam(":tipo", $datos["tipo"], PDO::PARAM_STR);
+    
+            if ($stmt->execute()) {
+                return "ok";
+            } else {
+                return "error";
+            }
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+}
