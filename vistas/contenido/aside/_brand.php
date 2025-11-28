33a6ffbbe3602f7ca23e183abba7ac48c525d480
Javier Pineda <javierpineda85@gmail.com>
2025-11-28 17:18:46 -0300

    Corrección en marca
    Ahora toma los datos del sistema

--- vistas/contenido/aside/_brand.php
+++ vistas/contenido/aside/_brand.php
@@ -1,4 +1,4 @@
 <a href="index.php" class="brand-link">
-  <img src="img/logo2024.png" alt="SPEC Logo" class="brand-image img-circle elevation-3">
-  <span class="brand-text font-weight-light">SPEC</span>
+  <img src="img/logo2024.png" alt="<?= htmlspecialchars($config['system_name'] ) ?> Logo" class="brand-image img-circle elevation-3">
+  <span class="brand-text font-weight-light"><?= htmlspecialchars($config['system_name'] ) ?></span>
 </a>
